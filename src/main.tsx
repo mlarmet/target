a8d424351c1ea2fd35ffa3a4ea7caf615404504f
Maxence LARMET <maxencelarmet@orange.fr>
2026-04-20 17:41:20 +0200

    Add material symbols

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
+import "material-symbols/outlined.css";
+
 import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
